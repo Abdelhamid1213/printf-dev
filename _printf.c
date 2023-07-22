610b21de4eb19cc6dbcc242765b64ed782995c03
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-22 09:21:33 +0100

    Update `_printf.c`

--- _printf.c
+++ _printf.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _printf - format and print data
+ *
+ * @format: controls the output
+ *
+ * Return: lenght of string printed
+ */
+
+int _printf(const char *format, ...)
+{
+	;
+}
