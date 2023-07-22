5607c7a531f44eb4d3c00a0f10b75a38e4ac51aa
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-07-22 10:40:15 +0100

    Add `_puts()`

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: string to get printed
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
--- main.h
+++ main.h
@@ -6,5 +6,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+void _puts(char *str);
 
 #endif
